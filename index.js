dfe5a69c979dc3150fb6b082a286b1d2fd725ed1
janekbartnicki <jachu01@poczta.onet.pl>
2022-07-24 11:57:45 +0200

    1

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const fetchData = async (query) => {
+const fetchData = async query => {
     const response = await axios.get('http://www.omdbapi.com', {
         params: {
             apikey: '35e75b8a',
             s: query
         }
     });
-    console.log(response.data)
+    return response.data.Srearch;
 }
 
 const input = document.querySelector('input');
-input.addEventListener('input', (event) => {
-    fetchData(event.target.value);
-})
+
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+    console.log(movies)
+}
+input.addEventListener('input', debunce(onInput, 500));
